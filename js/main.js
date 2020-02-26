6e183ed484b488ff036fbbcd3f8a92ed43de8df5
Dimash <asqw0@bk.ru>
2020-02-26 21:53:42 +0600

    add more updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 'use strict'
+
+document.body.addEventListener( 'load', function(){
+    let
+        modal = document.querySelector('#modal'),
+        actionList = document.querySelectorAll('#action_list')    
+    ;
+
+    for(let i = 0; i < actionList.length; i++){
+        actionList[i].addEventListener('click', function(){
+            modal.classList.remove('display_block');  
+            console.log('jhjhj');         
+        })
+    }
+})
