fb3d6783d0be0524af66cb591fb6dba12b678c08
Giranda22_Git <aloxaeee@bk.ru>
2020-03-03 21:51:21 +0600

    update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
+'use strict'
 
 window.onload = function(){
     let
         modal = document.querySelector('#modal-body'),
         modal_close = document.querySelector('#modal-close'),
-        actionList = document.querySelectorAll('#action_list')  
+        actionList = document.querySelectorAll('#action_list')
     ;
-    console.log(actionList);
+    fetch('https://rickandmortyapi.com/api/character/')
+            .then(response => {
+                this.console.log(response);
+                if(response.status !== 200){
+                    this.console.log('sorry, error...');
+                } else {
+                    return response.json();
+                }
+            })
+            .then(json => renderList(json))
+            .catch(err => this.console.log(err));
+    
+    function renderList(data){
+        let 
+            rawData = data,
+            list = [...rawData.results],
+            info = rawData.info
+        ;
+        console.log(list);
+
+        document.querySelector('.total').textContent = info.count;
+
+    }
+    
     for(let i = 0; i < actionList.length; i++){
         actionList[i].addEventListener('click', function(){
             modal.classList.remove('display_none');
