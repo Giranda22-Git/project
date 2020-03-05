264e2d6f536ef4039cbeb7d8b6bd5048eaa19377
Giranda22-Git <aloxaeee@bk.ru>
2020-03-05 18:23:39 +0600

    update

--- js/main.js
+++ js/main.js
@@ -4,39 +4,101 @@ window.onload = function(){
     let
         modal = document.querySelector('#modal-body'),
         modal_close = document.querySelector('#modal-close'),
-        actionList = document.querySelectorAll('#action_list')
+        right_array = document.querySelector('.right-array > span'),
+        left_array = document.querySelector('.left-array > span'),
+        page_index = 1,
+        max_pages = 0,
+        loaded = 0,
+        max_loaded_page = 1,
+        time_machine = 0
     ;
-    fetch('https://rickandmortyapi.com/api/character/')
+    
+    loadData(page_index, 'default');
+
+    function loadData(page, road){
+        fetch('https://rickandmortyapi.com/api/character/?page=' + page)
             .then(response => {
-                this.console.log(response);
+                console.log(response);
                 if(response.status !== 200){
-                    this.console.log('sorry, error...');
+                    console.log('sorry, error...');
                 } else {
+                    clearList();
                     return response.json();
                 }
             })
-            .then(json => renderList(json))
-            .catch(err => this.console.log(err));
+            .then(json => renderList(json, road))
+            .catch(err => console.log(err));
+    }
     
-    function renderList(data){
+    function renderList(data, road){
         let 
             rawData = data,
-            list = [...rawData.results],
+            list = rawData.results,
             info = rawData.info
         ;
-        console.log(list);
+        console.log(page_index + "      " + max_loaded_page + "        " + time_machine);
 
-        document.querySelector('.total').textContent = info.count;
+        if(loaded <= info.count && road != 'left' && page_index == max_loaded_page && page_index > time_machine)
+            loaded += list.length;
+        max_pages = info.pages;
 
-    }
-    
-    for(let i = 0; i < actionList.length; i++){
-        actionList[i].addEventListener('click', function(){
-            modal.classList.remove('display_none');
-            modal.classList.add('display_flex');
-            modal_close.classList.remove('display_none');
+        document.querySelector('.allCurrent').textContent = info.count;
+        document.querySelector('.current').textContent = loaded;
+        document.querySelector('.max-pages').textContent = info.pages;
+        document.querySelector('.current-pages').textContent = page_index;
+
+
+        const listContainer = document.querySelector('.relative_front_sec1');
+
+        list.forEach((el, i, arr) => {
+
+            listContainer.innerHTML += `
+                <li id="item">${el.id} &nbsp;&nbsp;&nbsp; ${el.name}</li>
+            `
         })
+
+        let actionList = document.querySelectorAll('#item');
+
+        for (let i = 0; i < actionList.length; i++) {
+            actionList[i].addEventListener('click', function(){
+                modal.classList.remove('display_none');
+                modal.classList.add('display_flex');
+                modal_close.classList.remove('display_none');
+            })
+        }
+
+        time_machine = page_index;
     }
+
+    function clearList(){
+        let actionList = document.querySelectorAll('#item');
+
+        for (let i = 0; i < actionList.length; i++)
+            actionList[i].remove();
+    }
+
+    right_array.addEventListener('click', function(){
+        if(page_index + 1 <= max_pages)
+        {
+            if(page_index == max_loaded_page)
+                max_loaded_page++;
+            page_index++;
+            loadData(page_index, 'right');
+            
+        }
+    })
+
+    left_array.addEventListener('click', function(){
+        if(page_index - 1 > 0)
+        {
+            page_index--;
+            
+            loadData(page_index, 'left');
+        }
+    })
+
+    
+    
 
     modal_close.addEventListener('click', function(){
         modal_close.classList.add('display_none');
